55a9895aa624e81a49e84c7f1397f8f528faa6a1
yatish singhal <yatish0401@gmail.com>
2023-07-27 22:14:52 +0530

    created Driver class for claciImpl

--- calci.java
+++ calci.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 interface calci
 {
  int getSum(int n1 , int n2);
@@ -9,3 +10,18 @@ public int getSum(int n1 , int n2)
 return n1+n2;
 }
 }
+public class calci Driver {
+public static void main(String[] args)
+{
+Scanner s = new Scanner(System.in);
+System.out.println("enter 2 numbern to find sum");
+
+int n1= s.nextInt();
+int n2= s.nextInt();
+claciImpl c = new claciImpl();
+int sum = c.getSum(n1,n2);
+System.out.println(n1+"+"+n2+"="+sum);
+}
+}
+}
+
